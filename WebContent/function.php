7b0b14fccecb5aaf4d581b20d34a1777a28b56d1
Ethan N. Ha <80878848+ethannha@users.noreply.github.com>
2021-11-15 21:40:50 -0800

    Created function.php and implemented image shifting left, right, shuffle methods. (Pair-programmed with Steven and Isaiah)

---
+++ WebContent/function.php
@@ -0,0 +1,66 @@
+<?php
+
+function displayPicture($conn)
+{
+    foreach($_SESSION['saveArray'] as $value)
+    {
+        // write query for all entry_details
+        $sql = "Select * from objectslist where id = '$value'";
+        // make query and get result
+        $result = mysqli_query($conn, $sql);
+        if ($result->num_rows > 0)
+        {
+            echo '<div class=container>';
+            echo '<div class="column">';
+            while ($row = mysqli_fetch_assoc($result))
+            {
+            echo '<img src="'.$row['link'].'">';
+            }
+            echo '</div>';
+            echo '</div>';
+            //print_r($row);       //prints out all the data in $row (a certain row in the database)
+        }
+        else
+        {
+            header("Location: demo.php?error=obtaining picture");
+            echo "<script>alert('Error getting pictures');</script>";
+        }
+    //echo "ID of picture: ".$value.' ';
+    }
+}
+
+function shiftLeft($conn)
+{
+    $firstvalue = reset($_SESSION['saveArray']);  //set the internal pointer of an array to its first element
+    $firstkey = key($_SESSION['saveArray']);      //takes key according to reset's pointer
+    
+    $arrayElement = array($firstkey=>$firstvalue);  //stores that one element from pointer
+    
+    array_shift($_SESSION['saveArray']);  //shift to remove first element
+    
+    $arrayElement = array_merge($_SESSION['saveArray'],$arrayElement);    //merge-add arrayElement to the end of original array since we shift it LEFT
+
+    $_SESSION['saveArray'] = $arrayElement;
+}
+
+function shiftRight($conn)
+{
+    $lastvalue = end($_SESSION['saveArray']);
+    $lastkey = key($_SESSION['saveArray']);
+    
+    $arr1 = array($lastkey=>$lastvalue);
+    
+    array_pop($_SESSION['saveArray']);
+    
+    $arr1 = array_merge($arr1,$_SESSION['saveArray']);
+
+    $_SESSION['saveArray'] = $arr1;
+}
+
+
+
+function shufflePicture($conn)
+{
+    shuffle($_SESSION['saveArray']);
+}
+?>
--- WebContent/presentation.php
+++ WebContent/presentation.php
@@ -1,10 +1,97 @@
 <?php
-
-require_once 'db_conn.php';
-
-if (isset($_POST['check']))
-{
-    
-}
+session_start();
+require "db_conn.php"; // Making sure this file has the connection to our database
+require_once "function.php";
 
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  <!-- Connecting to bootstrap -->
+    <link rel="stylesheet" type="text/css" href="css/style.css"> <!-- Specific file for all customization-->
+
+
+  </head>       <!-- End of Head -->
+
+  <body style="font-family:arial;">
+    <section class="nbar" style="background-color:#eee;">
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark justify-content-center">
+        
+        <a href="Home.php" class="navbar-brand mr-auto">Error 404</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+  
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        
+          <ul class="navbar-nav justify-content-center">
+            <li class="nav-item active">
+              <a class="nav-link" href="Home.php">Home<span class="sr-only"></span></a>
+            </li>
+            <?php if(isset($_SESSION['username']) && isset($_SESSION['id'])) { ?>
+              <li class="nav-item"> <a class="nav-link" href="Library.php">Library</a> </li>
+            <?php } ?>
+            <li class="nav-item">
+              <a class="nav-link" href="demo.php">Presentation</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="About.php">About</a>
+            </li>
+          </ul>
+  
+          <!--- Checks if user is on an account, and if they are display their username and the logout option --->
+          <?php if(isset($_SESSION['username']) && isset($_SESSION['id'])) { ?>
+          <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
+              <li class="nav-item">
+                  <p class = "nav-link" style="text-decoration: none">
+                  <?php echo $_SESSION['username']; ?>
+                  </p>
+              </li>
+            <li class="nav-item">
+              <a class="nav-link" href="logout.php">Logout</a>
+            </li>
+          </ul>
+          <?php } else {?> <!-- ENDING IF STATEMENT -->
+          <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
+            <li class="nav-item">
+              <a class="nav-link" href="login.html">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="SignUp.html">Sign Up</a>
+            </li>
+          </ul>
+          <?php } ?>
+        </div>
+      </nav>
+    </section>
+
+    <?php
+        displayPicture($conn);
+    ?>
+
+    <form method="POST">
+        <input type="submit" name="left" value="<< Left">
+        <input type="submit" name="right" value="Right >>">
+        <input type="submit" name="shuffle" value="Shuffle">
+    </form>
+
+    <?php
+    if(isset($_POST['left']))
+    {
+      shiftLeft($conn);
+    }
+    if(isset($_POST['right']))
+    {
+      shiftRight($conn);
+    }
+    if(isset($_POST['shuffle']))
+    {
+      shufflePicture($conn);
+    }
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>    <!-- Getting the bundles of Bootstrap -->
+    
+  </body>     <!-- End of Body -->
+</html>
