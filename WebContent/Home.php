e922e60756bbe4683f915346c43285da2653a4b0
Ethan N. Ha <80878848+ethannha@users.noreply.github.com>
2021-11-19 14:39:38 -0800

    Revert "Merge branch 'main' of https://github.com/OtaSteven/Error-404"
    This reverts commit 02581972e56e3a016882fa1c8f7d89238bfc2d19, reversing
    changes made to d78541b30eca57c57679855176b8bd892ef4613a.

--- WebContent/Home.php
+++ WebContent/Home.php
@@ -15,16 +15,13 @@ session_start();
     img 
     {
       display: block;
-      padding-top: 5%;
       margin-left: auto;
       margin-right: auto;
-      width: 65%;
+      width: 45%;
       height: auto;
-      max-width: 75%;
-      opacity: 0.35;
+      opacity: 0.5;
       border-radius: 5px;
       z-index: -1;
-      background-size: contain;
     }
   </style>
 
@@ -85,11 +82,11 @@ session_start();
   </section>
 
 <!-- landing page welcome and body -->
-    <header class="container-fluid" style="height: 100vh;">
+    <header class="head container-fluid">
       <img class="blur" src="images/tempgif.gif" alt="temp">
       <div class="welcome">
         <h1 class="fw-bolder">Welcome to the Web App</h1>
-        <p class="fw-bold">A simple page to help you get started using the app. With the buttons below, you can Get Started and sign up or try our demo. Scroll down for more information.</p>
+        <p class="fw-bold">A simple page to help you get started using the app</p>
         <a class="btn btn-primary btn-lg px-4 me-sm-3" href="SignUp.html" target="_blank">Get Started</a>
         <a class="btn btn-outline-dark btn-lg px-4" href="demo.php" target="_blank">Demo</a> <!-- links to presentation page -->
       </div>
@@ -103,14 +100,14 @@ session_start();
       </div>
 
       <div class="row justify-content-md-center">
-        <div class="col col-lg-3">
+        <div class="col col-lg-2">
           <h3>How it Works</h3>
-          <p>Using a set of at least 3 images selected by the user, the group of images rotate in order to help students learn. You can test a preview using our demo above.</p>
+          <p>Using specific sets of images chosen by the user, the group of 6 images rotate  with the same format.</p>
         </div> 
-        <div class="col col-lg-2"></div> <!-- empty to make space between how and purpose (maybe add image -->
-        <div class="col col-lg-3">
-          <h3>More Information</h3>
-          <p>For more options, sign up for our web app today and find features to help you help your students. With features such as access to the library database.</p>
+        <div class="col col-lg-4"></div> <!-- empty to make space between how and purpose (maybe add image -->
+        <div class="col col-lg-2">
+          <h3>Purpose</h3>
+          <p>Designed to help ABAs, this web app is made with their purpose to help students.</p>
         </div>
       </div>
     </div>
