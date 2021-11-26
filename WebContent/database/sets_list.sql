291910f022c4f3e000a6ce47438c43d37ed5ad34
Ethan N. Ha <80878848+ethannha@users.noreply.github.com>
2021-11-25 21:57:24 -0800

    Added sets_list.sql database file to store sets for the set builder feature. Has ID, setName, 12 links to store individually in different columns. No row entries currently on this import file.

---
+++ WebContent/database/sets_list.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 26, 2021 at 05:05 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `projectaccount`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sets_list`
+--
+
+CREATE TABLE `sets_list` (
+  `ID` int(11) NOT NULL,
+  `setName` varchar(255) NOT NULL,
+  `link1` varchar(255) NOT NULL,
+  `link2` varchar(255) NOT NULL,
+  `link3` varchar(255) NOT NULL,
+  `link4` varchar(255) NOT NULL,
+  `link5` varchar(255) NOT NULL,
+  `link6` varchar(255) NOT NULL,
+  `link7` varchar(255) NOT NULL,
+  `link8` varchar(255) NOT NULL,
+  `link9` varchar(255) NOT NULL,
+  `link10` varchar(255) NOT NULL,
+  `link11` varchar(255) NOT NULL,
+  `link12` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `sets_list`
+--
+ALTER TABLE `sets_list`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `sets_list`
+--
+ALTER TABLE `sets_list`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
