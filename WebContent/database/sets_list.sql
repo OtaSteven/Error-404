cbf63d6371a979f49ab5681fc852bb09ae7a59fb
Ethan N. Ha <80878848+ethannha@users.noreply.github.com>
2021-12-08 15:37:19 -0800

    Deleted the id#0 N/A entry row in the sets_list.sql database file which was no longer necessary.

--- WebContent/database/sets_list.sql
+++ WebContent/database/sets_list.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 05, 2021 at 02:59 AM
+-- Generation Time: Dec 09, 2021 at 12:34 AM
 -- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.12
+-- PHP Version: 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -49,7 +49,6 @@ CREATE TABLE `sets_list` (
 --
 
 INSERT INTO `sets_list` (`ID`, `setName`, `link1`, `link2`, `link3`, `link4`, `link5`, `link6`, `link7`, `link8`, `link9`, `link10`, `link11`, `link12`) VALUES
-(0, 'N/A', '', '', '', '', '', '', '', '', '', '', '', ''),
 (1, 'Test 1', 'https://gray-wmctv-prod.cdn.arcpublishing.com/resizer/nsgGaJW2V-VSsC4fl6Xu6vYVTC8=/800x600/smart/filters:quality(70)/cloudfront-us-east-1.images.arcpublishing.com/gray/5V62QS6ZHNF6DBA7656F3RS4UY.jpg', 'https://0201.nccdn.net/4_2/000/000/00d/f43/ankle---foot-specialists-of-conroe--the-woodlands_causes-and-sol.jpeg', 'https://images.heb.com/is/image/HEBGrocery/000466634', 'https://www.kindpng.com/picc/m/165-1658328_fist-thumb-arm-clip-art-upper-arm-clipart.png', 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg', 'https://www.sacmag.com/wp-content/uploads/sites/50/2021/04/bagel-bunch.jpg', 'https://m.media-amazon.com/images/I/81UbNUfvstL._AC_UY445_.jpg', '', 'https://m.media-amazon.com/images/I/81S+7h513XL._AC_SL1500_.jpg', 'https://cdn.mos.cms.futurecdn.net/42E9as7NaTaAi4A6JcuFwG-1200-80.jpg', 'https://nypost.com/wp-content/uploads/sites/2/2020/06/band-aid.jpg?quality=80&strip=all', 'https://i.guim.co.uk/img/media/9d481a988ce145ac381261ea4eb47094ab3ac2b0/0_1209_2136_1282/master/2136.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=5004755d749d1f12745dea55016c57cd'),
 (2, 'Test 2', 'https://gray-wmctv-prod.cdn.arcpublishing.com/resizer/nsgGaJW2V-VSsC4fl6Xu6vYVTC8=/800x600/smart/filters:quality(70)/cloudfront-us-east-1.images.arcpublishing.com/gray/5V62QS6ZHNF6DBA7656F3RS4UY.jpg', 'https://0201.nccdn.net/4_2/000/000/00d/f43/ankle---foot-specialists-of-conroe--the-woodlands_causes-and-sol.jpeg', 'https://images.heb.com/is/image/HEBGrocery/000466634', 'https://www.kindpng.com/picc/m/165-1658328_fist-thumb-arm-clip-art-upper-arm-clipart.png', 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg', 'https://www.sacmag.com/wp-content/uploads/sites/50/2021/04/bagel-bunch.jpg', 'https://m.media-amazon.com/images/I/81UbNUfvstL._AC_UY445_.jpg', '', 'https://m.media-amazon.com/images/I/81S+7h513XL._AC_SL1500_.jpg', 'https://cdn.mos.cms.futurecdn.net/42E9as7NaTaAi4A6JcuFwG-1200-80.jpg', 'https://nypost.com/wp-content/uploads/sites/2/2020/06/band-aid.jpg?quality=80&strip=all', 'https://i.guim.co.uk/img/media/9d481a988ce145ac381261ea4eb47094ab3ac2b0/0_1209_2136_1282/master/2136.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=5004755d749d1f12745dea55016c57cd');
 
